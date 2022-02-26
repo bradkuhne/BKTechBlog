6004dd2eb7684c349a066c1b8edc979a6a04be81
Brad Kuhne <bjkuhne@aol.com>
2022-02-26 15:29:58 -0600

    added route from lesson 14.5 and authorization from 14.5 to get server.js to start

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,10 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3001,
-      dialectOptions: {
+        dialectOptions: {
         decimalNumbers: true,
       },
+      port: 3306
     });
 
 module.exports = sequelize;
